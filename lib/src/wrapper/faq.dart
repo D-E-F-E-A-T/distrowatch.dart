32105b0bbdd65c6130c51ad8913223e9ec134697
Rodrigo Muniz <rodrigo.temiski1995@gmail.com>
2018-05-08 15:48:16 -0300

    .

--- lib/src/wrapper/faq.dart
+++ lib/src/wrapper/faq.dart
@@ -2,26 +2,20 @@ import 'package:distrowatch/distrowatch.dart';
 import 'package:distrowatch/src/util/html_parser.dart';
 import 'package:html/dom.dart';
 
-List<Map<String, String>> parseFAQList(String html) {
-  List<Package> listPackage = new List<Package>();
+Map<String, String> parseFAQList(String html) {
   Document document = parseStringToHTML(html);
   Element blockquote = document.getElementsByTagName('blockquote')[0];
   List<Element> textNodes = blockquote.children
     .where((Element e) => e.nodeType == Node.TEXT_NODE)
     .toList();
-  nodes.forEach((Element node) { listPackage.add(parseFAQ(node)); });
-  return listPackage;
+  Map<String, String> mapFAQ = parseFAQ(textNodes);
+  return mapFAQ;
 }
 
-Map<String, String> parseFAQ(Element el) {
-  String name = el.children[0].text;
-  String version = el.children[1].text;
-  String description = el.children[2].text;
-  String url = el.children[0].children[1].attributes['href'];
-  return new Package(
-    name: name,
-    version: version,
-    description: description,
-    url: url,
-  );
+Map<String, String> parseFAQ(List<Element> el) {
+  // String name = el.children[0].text;
+  // String version = el.children[1].text;
+  // String description = el.children[2].text;
+  // String url = el.children[0].children[1].attributes['href'];
+  return new Map<String, String>();
 }
